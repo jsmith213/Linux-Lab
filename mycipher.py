6f66d965ffcb34e0044b3d040678c1ce638694a3
jsmith213 <jasmineadriana213@gmail.com>
2026-04-27 16:10:10 -0400

    Update code to have only one line and cipher when click enter

--- mycipher.py
+++ mycipher.py
@@ -3,12 +3,12 @@ import sys
 shift = int(sys.argv[1])
 result = []
 
-for line in sys.stdin:
-    for ch in line:
-        if ch.isalpha():
-            c = ord(ch.upper()) - ord('A')
-            c = (c + shift) % 26
-            result.append(chr(c + ord('A')))
+line = sys.stdin.readline()
+for ch in line:
+    if ch.isalpha():
+        c = ord(ch.upper()) - ord('A')
+        c = (c + shift) % 26
+        result.append(chr(c + ord('A')))
 
 # Print in blocks of 5, 10 blocks per line
 blocks = [''.join(result[i:i+5]) for i in range(0, len(result), 5)]
